65e5a0119b134c5f342bd3ce2a8d6b6aa8473e8a
michael <econgamer@gmail.com>
2019-09-02 10:59:28 +0800

    Add Maven template project

---
+++ src/main/java/com/cherrypicks/mavenBuildTest/App.java
@@ -0,0 +1,13 @@
+package com.cherrypicks.mavenBuildTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
